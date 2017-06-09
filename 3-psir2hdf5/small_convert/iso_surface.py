c44f0444e5849c47d3fd060fee7ef89cd8b08cb3
Paul Young <yyang173@illinois.edu>
2017-06-09 10:36:08 -0700

    functionalized isosurf

--- 3-psir2hdf5/small_convert/iso_surface.py
+++ 3-psir2hdf5/small_convert/iso_surface.py
@@ -14,42 +14,73 @@ def psig_to_psir(psig,gvec,nx):
     return psir
 # end def
 
-if __name__ == '__main__':
-    # 3D image
-    from skimage import measure
-    from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+def isosurf(ax,vol,level=None):
+    """ draw iso surface of volumetric data on matplotlib axis at given level
+    Inputs:
+      ax: matplotlib axis with projection='3d' 
+      vol: 3D volumetric data as a numpy array (nx,ny,nz) 
+      level: value of iso surface
+    Output:
+      None
+    Effect:
+      draw on ax """
 
-    # read orbital
-    wf = PwscfH5()
-    wf.read('pwscf.pwscf.h5')
-    gvec = wf.get('gvectors')
-    psig_arr = wf.psig()
-    psir_arr = wf.psir()
+    nx,ny,nz = vol.shape
+    lmin,lmax = vol.mean(),vol.max()
 
-    psig = psig_arr[:,0] + 1j*psig_arr[:,1]
-    mypsir = psig_to_psir(psig,gvec,12)
-    psir = psir_arr[:,:,:,0] + 1j*psir_arr[:,:,:,1]
+    if level is None: # set level to average if none given
+        level = 0.5*(lmin*lmax)
+    else: # check isosurface level
+        if level<lmin or level>lmax:
+            raise RuntimeError('level must be >%f and < %f'%(lmin,lmax))
+        # end if
+    # end if
 
-    nx,ny,nz = psir.shape
-    #val = (psir.conj()*psir).real # min: -0.0304, max: 0.03562, then squre
-    val = (mypsir.conj()*mypsir).real
-    print val.min(),val.max()
+    # make marching cubes
     verts, faces, normals, values = measure.marching_cubes_lewiner(
-        val ,1.0)
+        vol, level)
 
-    fig = plt.figure()
-    ax  = fig.add_subplot(111,projection='3d')
-
+    # plot surface
     mesh = Poly3DCollection(verts[faces])
     mesh.set_edgecolor('k')
     ax.add_collection3d(mesh)
-
     ax.set_xlim(0,nx)
     ax.set_ylim(0,ny)
     ax.set_zlim(0,nz)
     ax.set_xlabel('x')
     ax.set_ylabel('y')
     ax.set_zlabel('z')
+# end def isosurf
+
+if __name__ == '__main__':
+    # 3D image
+    from skimage import measure
+    from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+    # read orbital
+    loc = {'ikpt':0,'ispin':0,'istate':0}
+
+    wf = PwscfH5()
+    wf.read('pwscf.pwscf.h5')
+    gvec = wf.get('gvectors')
+    psig_arr = wf.psig(**loc)
+    psir_arr = wf.psir(**loc)
+
+    psig = psig_arr[:,0] + 1j*psig_arr[:,1]
+    mypsir = psig_to_psir(psig,gvec,12)
+    psir = np.fft.fftshift( psir_arr[:,:,:,0] + 1j*psir_arr[:,:,:,1] )
+
+    nx,ny,nz = psir.shape
+    valr = (psir.conj()*psir).real # min: -0.0304, max: 0.03562, then squre
+    val  = (mypsir.conj()*mypsir).real
+
+    fig = plt.figure()
+    ax  = fig.add_subplot(121,projection='3d',aspect=1)
+    ax1 = fig.add_subplot(122,projection='3d',aspect=1)
+
+    isosurf(ax,val,1.0)
+    isosurf(ax1,valr,1e-3)
+
     plt.show()
 
 # end __main__
