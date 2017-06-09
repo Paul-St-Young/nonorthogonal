0b7ab608d9aa727be7a9b794d82c0bca84071051
Paul Young <yyang173@illinois.edu>
2017-06-09 14:44:42 -0700

    convert between psi_g and psi_r

--- 3-psir2hdf5/small_convert/iso_surface.py
+++ 3-psir2hdf5/small_convert/iso_surface.py
@@ -5,13 +5,23 @@ import matplotlib.pyplot as plt
 from pwscf_h5 import PwscfH5
 
 def psig_to_psir(psig,gvec,nx):
+    """ !!!! assuming cubic grid with nx points on each dimension """
     assert len(psig) == len(gvec)
-    kgrid = np.zeros([nx,nx,nx])
+    kgrid = np.zeros([nx,nx,nx],dtype=complex)
     for ig in range(len(gvec)):
         kgrid[tuple(gvec[ig])] = psig[ig]
     # end for
     psir = np.fft.fftshift( np.fft.ifftn(kgrid) ) * nx**3.
     return psir
+# end def
+
+def psir_to_psig(psir,gvec):
+    kgrid = np.fft.fftn( np.fft.fftshift(psir) )
+    psig  = np.zeros(len(gvec),dtype=complex)
+    for ig in range(len(gvec)):
+        psig[ig] = kgrid[tuple(gvec[ig])]
+    # end for
+    return psig/np.prod(psir.shape)
 # end def
 
 def isosurf(ax,vol,level=None):
@@ -29,7 +39,7 @@ def isosurf(ax,vol,level=None):
     lmin,lmax = vol.mean(),vol.max()
 
     if level is None: # set level to average if none given
-        level = 0.5*(lmin*lmax)
+        level = 0.5*(lmin+lmax)
     else: # check isosurface level
         if level<lmin or level>lmax:
             raise RuntimeError('level must be >%f and < %f'%(lmin,lmax))
@@ -58,7 +68,7 @@ if __name__ == '__main__':
     from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
     # read orbital
-    loc = {'ikpt':0,'ispin':0,'istate':0}
+    loc = {'ikpt':1,'ispin':0,'istate':0}
 
     wf = PwscfH5()
     wf.read('pwscf.pwscf.h5')
@@ -66,21 +76,36 @@ if __name__ == '__main__':
     psig_arr = wf.psig(**loc)
     psir_arr = wf.psir(**loc)
 
+    # original psi_r and psi_g
     psig = psig_arr[:,0] + 1j*psig_arr[:,1]
-    mypsir = psig_to_psir(psig,gvec,12)
     psir = np.fft.fftshift( psir_arr[:,:,:,0] + 1j*psir_arr[:,:,:,1] )
 
-    nx,ny,nz = psir.shape
-    valr = (psir.conj()*psir).real # min: -0.0304, max: 0.03562, then squre
-    val  = (mypsir.conj()*mypsir).real
+    # convert psi_r to psi_g and vice versa
+    mypsir = psig_to_psir(psig,gvec,12)
+    mypsig = psir_to_psig(psir,gvec)
 
+    nx,ny,nz = psir.shape
+    assert nx == 12
+    assert (nx == ny) and (ny == nz)
+
+    # return to original psig and psir
+    psig_from_mypsir = psir_to_psig(mypsir,gvec)
+    assert np.allclose( psig_from_mypsir.real,psig.real )
+
+    psir_from_mypsig = psig_to_psir(mypsig,gvec,nz)
+    # this will not be the same as psir because gvec cuts out some plane waves
+    # check visually
+
+    val  = (psir_from_mypsig.conj()*psir_from_mypsig).real
+    valr = (psir.conj()*psir).real
+    print val.min(),val.max()
+    print valr.min(),valr.max()
+   
     fig = plt.figure()
     ax  = fig.add_subplot(121,projection='3d',aspect=1)
     ax1 = fig.add_subplot(122,projection='3d',aspect=1)
-
-    isosurf(ax,val,1.0)
-    isosurf(ax1,valr,1e-3)
-
+    isosurf(ax,val)#,1.0)
+    isosurf(ax1,valr)#,1.0)
     plt.show()
 
 # end __main__
