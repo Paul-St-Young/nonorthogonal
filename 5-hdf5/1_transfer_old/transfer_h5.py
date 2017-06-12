7c8e9fbaffe5a5b46ce800e19990fa5af8d2a364
Paul Young <yyang173@illinois.edu>
2017-06-12 09:05:38 -0700

    hard-coded hdf5 transfer

--- 5-hdf5/1_transfer_old/transfer_h5.py
+++ 5-hdf5/1_transfer_old/transfer_h5.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import numpy as np
 import h5py
 from pwscf_h5 import PwscfH5
 
@@ -13,6 +14,50 @@ if __name__ == '__main__':
     new_fname = 'from_ref.h5'
     new = h5py.File(new_fname,'w')
 
+    # transfer metadata
+    # ====
+    # transfer version info.
+    new.create_dataset('application/code',data=['espresso'])
+    new.create_dataset('application/version',data=[4,1,4])
+    new.create_dataset('format',data=['ES-HDF'])
+    new.create_dataset('version',data=[2,1,0])
+
+    # transfer lattice (supercell group)
+    alat = 3.77945227
+    axes = alat*np.eye(3)
+    new.create_dataset('supercell/primitive_vectors',data=axes)
+
+    # transfer particle set (atoms group)
+    atoms = ['H','H']
+    new.create_dataset('atoms/number_of_atoms',data=[len(atoms)])
+
+    pos   = np.dot(np.array([[0,0,0],[0.5,0.5,0.5]]), axes)
+    assert len(atoms)==len(pos)
+    new.create_dataset('atoms/positions',data=pos)
+
+    species = np.unique(atoms)
+    new.create_dataset('atoms/number_of_species',data=[len(species)])
+
+    species_map = {'H':0,'He':1,'Li':2,'Be':3}
+    species_ids = [species_map[atom] for atom in atoms]
+    new.create_dataset('atoms/species_ids',data=species_ids)
+
+    # describe existing species (!!!! just do H for now)
+    sp0 = new.create_group('atoms/species_0')
+    sp0.create_dataset('name',data=['H'])
+    sp0.create_dataset('atomic_number',data=[1])
+    sp0.create_dataset('valence_charge',data=[1])
+
+    # transfer wavefunction data
+    # ====
+
+    # transfer orbital info.
+    new.create_dataset('electrons/number_of_electrons',data=[1,1])
+    new.create_dataset('electrons/number_of_kpoints',data=[8])
+    new.create_dataset('electrons/number_of_spins',data=[1])
+    new.create_dataset('electrons/psi_r_is_complex',data=[1])
+
+    # transfer orbitals (electrons group)
     kpt0 = new.create_group('electrons/kpoint_0')
     kpt0.create_dataset('gvectors',data=gvec)
     
@@ -25,3 +70,5 @@ if __name__ == '__main__':
             new.create_dataset(psig_path,data=psig_arr)
         # end for
     # end for
+
+# end __main__
