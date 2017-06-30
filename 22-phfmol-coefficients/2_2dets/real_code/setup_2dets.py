178aaf52db1ba931850b5d1a13d632c3923b05d2
Paul Young <yyang173@illinois.edu>
2017-06-30 15:17:47 -0700

    check pairs of determinants

---
+++ 22-phfmol-coefficients/2_2dets/real_code/setup_2dets.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import os
+import numpy as np
+
+def set_ci_coeff(fname,ci_coeff):
+  """ set the CI coefficients of a multideterminant input 'fname' """
+  ndet = len(ci_coeff)
+
+  from input_xml import InputXml
+  inp = InputXml()
+  inp.read(fname)
+
+  detlist_node = inp.find('.//detlist')
+  nsize = int( detlist_node.get('size') )
+  assert nsize == ndet
+
+  ci_nodes = detlist_node.findall('.//ci')
+  for idet in range(ndet):
+    ci_node = ci_nodes[idet]
+    ci_id   = ci_node.get('id')
+
+    try:
+      idx = int( ci_id.split('_')[-1] )
+      assert idx == idet
+    except:
+      raise RuntimeError('failed to check @id of ci, uncomment this line to skip the check')
+    # end try
+
+    ci_node.set( 'coeff',str(ci_coeff[idet]) )
+  # end for idet
+
+  inp.write(fname)
+  del inp
+# end def set_ci_coeff
+
+def change_submission_name(sub_file,name):
+  import os
+  os.system( 'sed -i "s/my_epic_name/%s/" %s' % (name,sub_file) )
+# end def
+
+
+if __name__ == '__main__':
+
+  template_dir = 'noqc_det34'
+  input_name   = 'vmc.xml'
+  sub_fname    = 'qmc.msub'
+  ndet = 5
+
+  from itertools import combinations
+  for (idet,jdet) in combinations(range(5),2):
+    run_dir = 'det%d%d'%(idet,jdet)
+    ci_coeff = np.zeros(ndet)
+    ci_coeff[idet] = 1.0
+    ci_coeff[jdet] = 1.0
+    if not os.path.isdir(run_dir):
+      os.system('cp -r %s %s' % (template_dir,run_dir) )
+      fname = os.path.join(run_dir,input_name)
+      set_ci_coeff(fname,ci_coeff)
+      change_submission_name( os.path.join(run_dir,sub_fname), run_dir )
+      os.system('cd %s; sbatch %s' % (run_dir,sub_fname))
+    # end if
+  # end for 
+
+# end __main__
